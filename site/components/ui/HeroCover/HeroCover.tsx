100e19b8fac2596df19b1f735faf2e8405c2a0a6
James Shiow <jamesshiow@gmail.com>
2022-10-30 01:10:41 +0800

    fix herocover responsiveness. ref: https://stackoverflow.com/questions/70077107/make-different-size-images-to-be-responsive-using-next-image

--- site/components/ui/HeroCover/HeroCover.tsx
+++ site/components/ui/HeroCover/HeroCover.tsx
@@ -12,17 +12,21 @@ interface HeroProps {
 const HeroCover: FC<HeroProps> = ({ className, children }) => {
   return (
     <div className="bg-transparent">
-      <Container className="max-w-[100vw] px-6">
-        <div className={s.root}>
-          <Image
-            src={'/bg_hero_main_cover.svg'}
-            alt={'hero-banner'}
-            loading={'eager'}
-            layout="fill"
-            priority={true}
-          />
-        </div>
-      </Container>
+      {/* <Container className="max-w-[100vw] px-6"> */}
+      <div className={s.root}>
+        <Image
+          sizes="(min-width: 768px) 80px, 60px"
+          src={'/bg_hero_main_cover.svg'}
+          alt={'hero-banner'}
+          loading={'eager'}
+          width={1389}
+          height={617}
+          layout={'responsive'}
+          priority={true}
+          className={s.image}
+        />
+      </div>
+      {/* </Container> */}
       <div className="bg-store-p-light mx-6 pt-6 pb-14 rounded-b-xl">
         {children}
       </div>
